fd5d89a313316cf495798ef3b1eac7fae3ec0c43
ekovalchick <131537827+ekovalchick@users.noreply.github.com>
2023-11-21 19:07:02 -0500

    Revert "Feature/lg"

--- server/models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./User');
-const Snap = require('./Snap');
-const Department = require('./Department');
-const Task = require('./Task');
-
-module.exports = { User, Snap, Department, Task };
